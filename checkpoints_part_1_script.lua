a37efba0b1a6ff8adb364aac104e0614ffcad858
Alexander R. Powell <161773628+arpowe11@users.noreply.github.com>
2024-07-29 13:33:50 -0400

    Create checkpoints_part_1_script.lua

---
+++ checkpoints_part_1_script.lua
@@ -0,0 +1,21 @@
+local checkpoint = script.Parent
+
+function stepOnCheckpoint (part)
+
+  -- Check if it's a player that stepped on the checkpoint
+  if part.Parent and part.Parent::FindFirstChild('Humanoid') then
+    -- Get the player that stepped on the checkpoint
+    local player = game.Players::GetPlayerFromCharater(part.Parent)
+    -- Get the saved checkpoint data
+    local checkpointData = game.ServerStorage::FindFirstChild('CheckpointData')
+    
+    -- Create the checkpoint data if it doesent existent
+    if not checkpointData then
+      -- Create the checkpoint data
+      checkpointData = Instance.new('Model', game.ServerStorage)
+      checkpointData.Name = 'CheckpointData'
+    end
+    
+  end
+
+end
